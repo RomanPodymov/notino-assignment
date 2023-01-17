d97541a467507c0ff29ac6a24be4e92007b25c59
Roman Podymov <podymfrombryansk@yandex.ru>
2023-01-17 19:48:46 +0100

    Fix build errors

--- Sources/NotinoAssets.swift
+++ Sources/NotinoAssets.swift
@@ -5,9 +5,9 @@ import UIKit
 public class NotinoAssets {
     public static let resourceBundle: Bundle = {
         let bundle = Bundle(for: NotinoAssets.self)
-        guard let resourceBundleURL = bundle.url(forResource: "NotinoAssets", withExtension: "bundle")
-        guard let resourceBundle = Bundle(url: resourceBundleURL)
-        return resourceBundle
+        let resourceBundleURL = bundle.url(forResource: "NotinoAssets", withExtension: "bundle")
+        let resourceBundle = Bundle(url: resourceBundleURL!)
+        return resourceBundle!
     }()
 
     public static var heart: UIImage {
@@ -19,17 +19,17 @@ public class NotinoAssets {
         let url = resourceBundle.url(forResource: "heart-bold", withExtension: "pdf")
         return drawPDFfromURL(url: url!)!
     }
-    
+
     public static var icoStarGrey: UIImage {
         let url = resourceBundle.url(forResource: "ico-star-grey", withExtension: "pdf")
         return drawPDFfromURL(url: url!)!
     }
-    
+
     public static var icoStarPink: UIImage {
         let url = resourceBundle.url(forResource: "ico-star-pink", withExtension: "pdf")
         return drawPDFfromURL(url: url!)!
     }
-    
+
     private static func drawPDFfromURL(url: URL) -> UIImage? {
         guard let document = CGPDFDocument(url as CFURL) else { return nil }
         guard let page = document.page(at: 1) else { return nil }
@@ -49,5 +49,4 @@ public class NotinoAssets {
         return img
     }
 }
-
 
